3124c129492110267e417ffbb6ddaadb6b3417f0
metaluxuryin-cloud <metaluxury.in@gmail.com>
2026-07-03 18:07:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ def test_call():
         "agent_phone_number_id": PHONE_ID,
         "recipients": [
             {
-                "phone_number": "+917990403189",
+                "phone_number": "+918460387151",
                 "conversation_initiation_client_data": {
                     "dynamic_variables": {
-                        "customer_name": "RIYAZ lOHIYA ",
+                        "customer_name": "AYAZ lOHIYA ",
                         "product_name": "CROCS LITERIDE 360 BLACK GREY COLOUR",
                         "size": "UK 9",
                         "quantity": "1",
